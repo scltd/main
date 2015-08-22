d49d3badaa103e64798ab1dca80b9d4f399a41c1
Adam Pokora <apokora@scsociety.com>
2015-08-22 21:10:07 +0200

    netty initial

---
+++ main/scjapi/src/main/java/com/scsociety/scjapi/io/network/DiscardDecoder.java
@@ -0,0 +1,19 @@
+package com.scsociety.scjapi.io.network;
+
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+public class DiscardDecoder extends ByteToMessageDecoder { // (1)
+	@Override
+	protected void decode(ChannelHandlerContext ctx, ByteBuf in,
+			List<Object> out) { // (2)
+		if (in.readableBytes() < 4) {
+			return; // (3)
+		}
+
+		out.add(in.readBytes(4)); // (4)
+	}
+}
